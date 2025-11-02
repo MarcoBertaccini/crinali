e9ac7da55ae87f26ba32a8f3f65ec2a169f02a9f
MarcoBertaccini <marco.bertaccini9@gmail.com>
2025-11-02 17:52:44 +0100

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/crinali/' : '/',
+  base: '/crinali/',
   plugins: [react()],
-  optimizeDeps: { exclude: ['lucide-react'] },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: { 'react-vendor': ['react', 'react-dom'] }
-      }
-    },
-    minify: 'esbuild',
-    cssMinify: true
-  }
-});
+})
