573f81268fd633f5f91b8484bc7e92c08bc9f7f4
MarcoBertaccini <marco.bertaccini9@gmail.com>
2025-11-12 17:44:27 +0100

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -46,7 +46,7 @@ export default function Home({ onNavigate }: HomeProps) {
             onClick={() => onNavigate('partner')}
               className="w-auto bg-white/10 backdrop-blur-sm hover:bg-white/20 text-white px-10 py-4 rounded-lg font-semibold text-lg transition-all border-2 border-white/50 min-h-[44px]"
 
-  }}
+  
             >
               Eventi
             </button>
