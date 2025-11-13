e0ac21dc4670309a0dac071c950f42c17876d6e5
MarcoBertaccini <marco.bertaccini9@gmail.com>
2025-11-13 22:50:29 +0100

    Updated Partner.tsx

--- src/pages/Partner.tsx
+++ src/pages/Partner.tsx
@@ -155,7 +155,7 @@ const minorPartners = [
         </div>
       
           
-       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 sm:gap-7 max-w-3xl mx-auto px-4">
+       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 sm:gap-7 max-w-3xl mx-auto px-2">
             {minorPartners.map((partner, index) => (
               <div
                 key={index}
