941596f448979af08ef6a02754e8ac4576aa0234
MarcoBertaccini <marco.bertaccini9@gmail.com>
2025-11-13 21:28:34 +0100

    Updated Partner.tsx

--- src/pages/Partner.tsx
+++ src/pages/Partner.tsx
@@ -154,7 +154,7 @@ const minorPartners = [
           </div>
 
            <div className="mt-20">
-
+ </div>
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 sm:gap-8 max-w-5xl mx-auto">
             {minorPartners.map((partner, index) => (
               <div
