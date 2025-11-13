19b2fb52b0c834a4b2de8f27db2ff53674001ca9
MarcoBertaccini <marco.bertaccini9@gmail.com>
2025-11-13 22:18:33 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
+  base: '/', // lascia così se usi dominio personalizzato (es. crinalirunning.it)
   plugins: [react()],
+  build: {
+    outDir: 'dist',       // dice a Vite dove mettere i file compilati
+    emptyOutDir: true,    // pulisce la cartella prima di ricrearla
+  },
 })
